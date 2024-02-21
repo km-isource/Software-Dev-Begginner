f934553b816d0328086086aba96639755f0de618
iSource <127972198+km-isource@users.noreply.github.com>
2024-02-21 19:52:34 +0300

    User Input

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+
+name = input("Enter Your Name: ")
+age = int(input("Whats Your Age: "))
+location = input("Enter Your Location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
