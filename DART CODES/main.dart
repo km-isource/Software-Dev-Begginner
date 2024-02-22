1a4cdbbea66d11b55d030876086958297bc2958d
iSource <127972198+km-isource@users.noreply.github.com>
2024-02-22 19:49:14 +0300

    Dart Practise

--- DART CODES/main.dart
+++ DART CODES/main.dart
@@ -1,3 +1,23 @@
-void main() async {
-    print("Hello World");
+void main(){
+  //Comments
+  //Variables
+  String name ="Jack";
+  int age=56;
+  String nationality = "Kenyan";
+  bool ismarried = false;
+
+  /* Multi Line Comments
+  String name ="Jack";
+  int age=56;
+  String nationality = "Kenyan";
+  bool ismarried = false;
+  */
+
+  //Printing The Output
+  print(name);
+  print(age);
+  print(nationality);
+  print(ismarried);
+
+  print("Hello i am $name and im $age years old. I am $nationality");
 }
