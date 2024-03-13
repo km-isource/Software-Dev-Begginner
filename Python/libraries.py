0a88ff13ef6e163bd74c129461c67964f3ee7d31
iSource <127972198+km-isource@users.noreply.github.com>
2024-03-13 14:47:47 +0300

    Python Libraries

--- Python/libraries.py
+++ Python/libraries.py
@@ -1,3 +1,13 @@
 import numpy as np
-arr = np.array([10,20,30,40,50])
-print(arr)
+# arr = np.array([10,20,30,40,50])
+# print(arr)
+
+# a = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
+# print(a)
+
+a = np.array([1,2,3,4,5], ndmin = 2)
+print(a)
+
+
+
+
