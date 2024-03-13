76cc8beda09df7ae1656cf89958cec5f3c635c4b
iSource <127972198+km-isource@users.noreply.github.com>
2024-03-13 15:09:00 +0300

    Python Libraries

--- Python/libraries.py
+++ Python/libraries.py
@@ -1,12 +1,31 @@
 import numpy as np
+# creating an array using np
 # arr = np.array([10,20,30,40,50])
 # print(arr)
 
+# more than one dimension
 # a = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
 # print(a)
 
-a = np.array([1,2,3,4,5], ndmin = 2)
+# minimum dimension
+# a = np.array([1,2,3,4,5], ndmin = 2)
+# print(a)
+
+# dtype parametre
+# a = np.array([1,2,3,4,5], dtype= complex)
+# print(a)
+
+a = np.arange(0,60,5)
+a = a.reshape(3,4)
+print("Original Array Is")
 print(a)
+print("\n")
+print("Modified array is")
+for x in np.nditer(a):
+    print(x)
+
+
+
 
 
 
